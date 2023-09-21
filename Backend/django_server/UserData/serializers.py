0dc686d0fcd8e3b3f98e20dbbf14ef5d09f0b6b4
himanididu@5487 <sudarshanshrivastava7@gmail.com>
2023-09-21 22:27:56 +0530

    serverend created

--- Backend/django_server/UserData/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class User_Serializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields=('name','mobile','password')
-        
